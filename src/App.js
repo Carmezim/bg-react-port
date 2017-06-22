0ce71bcf65a8a20911287abe552d44560901d0bc
Carmezim <carmezim.filho@gmail.com>
2017-06-22 02:57:03 +0100

    mock admin page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Admin from './components/Admin';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Main page</h1>
-      </div>
+      <Router> 
+        <div className="App">
+          <Link to="/admin"></Link>
+          <Route path="/admin" component={Admin} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Admin extends Component {
+	render() {
+		return(
+			<div>
+				<h1>Admin Page</h1>
+				<h2>Here is where login will show</h2>
+			</div>
+		);
+	}
+}
+
+export default Admin;
