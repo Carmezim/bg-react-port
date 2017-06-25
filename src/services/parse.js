568d8ddccfcff6495ebb444d5d26b9568575af65
Carmezim <carmezim.filho@gmail.com>
2017-06-25 23:55:40 +0100

    adding current user and logout methods

--- src/services/parse.js
+++ src/services/parse.js
@@ -7,17 +7,6 @@ class ParseService {
 	}
 
 
-	login(username, password) {
-		Parse.User.logIn(username, password, {
-			success: function(user) {
-				alert('Login successful');
-				return true;
-			},
-			error: function(user, error) {
-				console.log('Login failed');
-			}
-		});
-	}
 
 	// this function is called on 'sagas.js' and returns the response
 	// that will be inserted into the corresponding dispatched action e.g.
@@ -26,6 +15,36 @@ class ParseService {
 		// TODO: check how emails for newsletter will be handled by Parse
 		alert(`Mocking email ${emailResponse.email} submission for newsletter`);
 		return emailResponse;
+	}
+
+
+	// Log user in returning session token on a successful call.
+	login(username, password) {
+		Parse.User.logIn(username, password, {
+			success: function(user) {
+				alert('Login successful');
+				return Parse.User.getSessionToken();
+			},
+			error: function(user, error) {
+				console.log('Login failed');
+				// return false;
+			}
+		});
+	}
+
+
+	// set current user through session token
+	setCurrentUser(token) {
+		Parse.User.become(token.toString()).then( (user) => {
+			console.log('Token was validated and current user is now set.');
+		}, (error) => {
+			console.error('Token could not be validated');
+		});
+	}
+
+	// Log user out
+	logOut() {
+		Parse.User.logOut()
 	}
 
 }
