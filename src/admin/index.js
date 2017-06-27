5ec6fdcb082e96877a6f1caf1d9f70c774a2b404
Carmezim <carmezim.filho@gmail.com>
2017-06-27 06:05:51 +0100

    updating admin component

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import ParseService from '../services/parseAPI';
 
 
-const Admin = () => {
-		return <div><h1>Admin Panel</h1></div>
-};
+class Admin extends Component {
+
+	static isPrivate = true;
+
+	submit() {
+		console.log('test')
+		ParseService.logOut()
+	}
+
+	render() {
+
+		return (
+			<div>
+				<h1>Admin Panel</h1>
+				<button onSubmit={this.submit()}>Logout</button>
+			</div>
+		);
+	}
+}
+
 
 export default Admin;
