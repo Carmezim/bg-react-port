53e5fd82766b40f7690f037dd71cf839ac309737
Carmezim <carmezim.filho@gmail.com>
2017-06-29 20:07:45 +0100

    adding client to index-reducer

--- src/index-reducer.js
+++ src/index-reducer.js
@@ -1,12 +1,16 @@
-import { combineReducers } from 'redux'
-import login from './login/reducer';
+import { combineReducers } from 'redux';
+import client from './client/reducer';
 import signup from './signup/reducer';
+import login from './login/reducer';
+import bookEvents from './dashboard/reducer';
 import { reducer as form } from 'redux-form';
 
 
 const IndexReducer = combineReducers({
+	client,
 	signup,
 	login,
+	bookEvents,
 	form,
 });
 
