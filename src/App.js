e2ac7e73695e742e16fe822a02c7904fb13e3df8
Carmezim <carmezim.filho@gmail.com>
2017-06-30 00:55:31 +0100

    show logout button when token present

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PrivateRoute from './common/PrivateRoute/AuthRoute'
-import { Route , Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import ParseService from './services/parseAPI';
-
-
-
 
 
 // Import all components
@@ -17,48 +14,56 @@ import Signup from './signup';
 import './App.css';
 
 
+// import actions
+import { unsetClient } from './client/actions'
+
+
 class App extends Component {
 
-	// static propTypes = {
-	// 	children: PropTypes.node,
-	// 	client: PropTypes.shape({
-	// 		token: PropTypes.object,
-	// 	}),
-	// };
-
-	clickHandler = () => {
-		ParseService.logOut()
+	static propTypes = {
+		children: PropTypes.node,
+		logout: PropTypes.func,
+		client: PropTypes.shape({
+			token: PropTypes.object,
+		}),
 	};
 
 
 	render() {
 
-		// const { client, submit } = this.props;
+		const { client: token } = this.props;
 
-    return (
-      <div className="App">
-        <div className="app-header">
-          {/*<h2>BookGig MainPage</h2>*/}
-        </div>
-        <section className="app-body">
+		return (
+			<div className="App">
+				<div className="app-header">
+					{/*<h2>BookGig MainPage</h2>*/}
+				</div>
+				<section className="app-body">
 					<Switch>
 						{/*<Route path="/admin" component={Login} />*/}
 						<PrivateRoute path="/admin" component={Login} />
 						<PrivateRoute path="/dashboard" component={Dashboard} />
-						<Route exact path="/" component={Signup} />
+						<Route path="/" component={Signup} />
 					</Switch>
 				</section>
-			<div className="logout-button">
-			<button onClick={() => this.clickHandler()}>
-					Logout
-			</button>
+				<div className="logout-button">
+					{!!token.token && (
+						<button onClick={this.props.unsetClient}>
+							{/*<button onClick={ParseService.logOut}>*/}
+						Logout
+						</button>
+					)}{console.log(token)}
+				</div>
 			</div>
-		</div>
-    );
-  }
+		);
+	}
 }
 
 
+const mapStateToProps = state => ({
+	client: state.client
+});
 
 
-export default (App);
+export default connect(mapStateToProps, { unsetClient })(App);
+
