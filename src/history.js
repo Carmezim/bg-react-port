ab032e851df17032b2ec7784e4db9b61ccf80636
Carmezim <carmezim.filho@gmail.com>
2017-06-30 01:02:39 +0100

    history update

--- src/history.js
+++ src/history.js
@@ -1,5 +1,5 @@
 // history.js
-import { createBrowserHistory } from 'history'
+import createBrowserHistory from 'history/createBrowserHistory';
 
 // configure, create, and export the project's history instance
 export default createBrowserHistory()
