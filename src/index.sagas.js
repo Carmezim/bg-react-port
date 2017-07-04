2a721c60b72445e0fd4f7b0d049f5c009a156a7a
Carmezim <carmezim.filho@gmail.com>
2017-07-04 02:23:46 +0100

    adding drag list saga to index-sagas

--- src/index.sagas.js
+++ src/index.sagas.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import SignupSaga from "./signup/sagas";
 import LoginSaga from "./login/sagas";
+import dndWatcher from "./draggableList/sagas";
 
 export default function* IndexSaga() {
 	yield all([SignupSaga(), LoginSaga()]);
