443ac24460ac9c753047d9a2c87b62e06cc7f323
Carmezim <carmezim.filho@gmail.com>
2017-07-04 02:33:13 +0100

    yet to implement dashboard saga

--- src/dashboard/sagas.js
+++ src/dashboard/sagas.js
@@ -1,1 +1,4 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+// import { call, put, takeLatest } from "redux-saga/effects";
+
+
+// TODO: implement dashboard saga handling event creation flow and remaining actions side effects
