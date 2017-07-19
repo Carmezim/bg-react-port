ad107af4ffeb52bf687a32ece007ebe624d83200
Carmezim <carmezim.filho@gmail.com>
2017-07-19 02:18:03 +0100

    trying navbar css

--- src/common/navigation/NavBar.js
+++ src/common/navigation/NavBar.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
+// import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
+
+import "./NavBar.css";
 
 const NavBar = props => {
 	return (
 		<div className="wide">
 			<ul className="tabs-menu ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
-				<a className="adult-tab-button">Adult</a>
+				<NavLink  className="adult-tab-button" exact to="/dashboard/adult">Adult</NavLink>
 				<li className="tab-group-adult tab-1 active ui-state-default ui-corner-top current ui-tabs-active ui-state-active">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/past">
 						Live
@@ -17,7 +19,7 @@ const NavBar = props => {
 						Hidden
 					</NavLink>
 				</li>
-					<li className="tab-group-adult tab-drafts-tab ui-state-default ui-corner-top">
+				<li className="tab-group-adult tab-drafts-tab ui-state-default ui-corner-top">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/drafs">
 						Drafts
 					</NavLink>
@@ -27,51 +29,75 @@ const NavBar = props => {
 						Curated Lists
 					</NavLink>
 				</li>
-				<a className="children-tab-button">Children</a>
+				<NavLink exact className="children-tab-button" to="/dashboard/children">Children</NavLink>
 				<li className="tab-group-children ui-state-default ui-corner-top">
 					<NavLink exact activeClassName="active" to="/dashboard/children/past">
 						Live
 					</NavLink>
 				</li>
 				<li className="tab-group-children ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/children/hidden">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/children/hidden"
+					>
 						Hidden
 					</NavLink>
 				</li>
 				<li className="tab-group-children tab-drafts-tab-children ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/children/drafs">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/children/drafs"
+					>
 						Drafts
 					</NavLink>
 				</li>
 				<li className="tab-group-children ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/children/curated">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/children/curated"
+					>
 						Curated Lists
 					</NavLink>
 				</li>
-				<a className="children-tab-button">Imports</a>
+				<NavLink exact className="children-tab-button" to="/dashboard/imports">Imports</NavLink>
 				<li className="tab-group-imports tab-waterstones ui-state-default ui-corner-top current ui-tabs-active ui-state-active ui-state-hover">
 					<NavLink exact activeClassName="active" to="/dashboard/imports/past">
 						Via Waterstones
 					</NavLink>
 				</li>
 				<li className="tab-group-imports tab-uploads ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/imports/hidden">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/imports/hidden"
+					>
 						Via Uploads
 					</NavLink>
 				</li>
 				<li className="tab-group-imports tab-uploads ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/imports/hidden">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/imports/hidden"
+					>
 						Via FTP Uploads
 					</NavLink>
 				</li>
-				<a className="children-tab-button">Settings</a>
+				<NavLink exact className="children-tab-button" to="/dashboard/settings">Settings</NavLink>
 				<li className="tab-group-settings ui-state-default ui-corner-top">
 					<NavLink exact activeClassName="active" to="/dashboard/settings/past">
 						Authors
 					</NavLink>
 				</li>
 				<li className="tab-group-settings ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/settings/hidden">
+					<NavLink
+						exact
+						activeClassName="active"
+						to="/dashboard/settings/hidden"
+					>
 						Categories
 					</NavLink>
 				</li>
