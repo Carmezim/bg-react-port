861eb345aa7e3b09f3eec287c3876193fcfbcf18
Carmezim <carmezim.filho@gmail.com>
2017-07-19 05:24:00 +0100

    sidebar expand when active

--- src/common/navigation/NavBar.js
+++ src/common/navigation/NavBar.js
@@ -8,67 +8,71 @@ const NavBar = props => {
 	return (
 		<div className="wide">
 			<ul className="tabs-menu ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all">
-				<NavLink  className="adult-tab-button" exact to="/dashboard/adult">Adult</NavLink>
-				<li className="tab-group-adult tab-1 active ui-state-default ui-corner-top current ui-tabs-active ui-state-active">
+				<NavLink
+					className="adult-tab-button"
+					activeClassName="active"
+					exact
+					to="/dashboard/adult"
+				>
+					Adult
+				</NavLink>
+				<li className="tab-group-adult tab-1 current active">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/past">
 						Live
 					</NavLink>
 				</li>
-				<li className="tab-group-adult ui-state-default ui-corner-top">
+				<li className="tab-group-adult">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/hidden">
 						Hidden
 					</NavLink>
 				</li>
-				<li className="tab-group-adult tab-drafts-tab ui-state-default ui-corner-top">
+				<li class="tab-group-adult">
+					<NavLink exact activeClassName="active" to="/dashboard/adult/hidden">
+						Past
+					</NavLink>
+				</li>
+				<li className="tab-group-adult tab-drafts-tab">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/drafs">
 						Drafts
 					</NavLink>
 				</li>
-				<li className="tab-group-adult tab-5 ui-state-default ui-corner-top">
+				<li className="tab-group-adult tab-5">
 					<NavLink exact activeClassName="active" to="/dashboard/adult/curated">
 						Curated Lists
 					</NavLink>
 				</li>
-				<NavLink exact className="children-tab-button" to="/dashboard/children">Children</NavLink>
-				<li className="tab-group-children ui-state-default ui-corner-top">
-					<NavLink exact activeClassName="active" to="/dashboard/children/past">
+				<NavLink exact className="children-tab-button" to="/dashboard/children">
+					Children
+				</NavLink>
+				<li className="tab-group-children">
+					<NavLink exact to="/dashboard/children/past">
 						Live
 					</NavLink>
 				</li>
-				<li className="tab-group-children ui-state-default ui-corner-top">
-					<NavLink
-						exact
-						activeClassName="active"
-						to="/dashboard/children/hidden"
-					>
+				<li className="tab-group-children">
+					<NavLink exact to="/dashboard/children/hidden">
 						Hidden
 					</NavLink>
 				</li>
-				<li className="tab-group-children tab-drafts-tab-children ui-state-default ui-corner-top">
-					<NavLink
-						exact
-						activeClassName="active"
-						to="/dashboard/children/drafs"
-					>
+				<li className="tab-group-children">
+					<NavLink exact to="/dashboard/children/drafs">
 						Drafts
 					</NavLink>
 				</li>
-				<li className="tab-group-children ui-state-default ui-corner-top">
-					<NavLink
-						exact
-						activeClassName="active"
-						to="/dashboard/children/curated"
-					>
+				<li className="tab-group-children">
+					<NavLink exact to="/dashboard/children/curated">
 						Curated Lists
 					</NavLink>
 				</li>
-				<NavLink exact className="children-tab-button" to="/dashboard/imports">Imports</NavLink>
-				<li className="tab-group-imports tab-waterstones ui-state-default ui-corner-top current ui-tabs-active ui-state-active ui-state-hover">
+				<NavLink exact className="imports-tab-button" to="/dashboard/imports">
+					Imports
+				</NavLink>
+				<li className="tab-group-imports">
 					<NavLink exact activeClassName="active" to="/dashboard/imports/past">
 						Via Waterstones
 					</NavLink>
 				</li>
-				<li className="tab-group-imports tab-uploads ui-state-default ui-corner-top">
+				<li className="tab-group-imports">
 					<NavLink
 						exact
 						activeClassName="active"
@@ -77,7 +81,7 @@ const NavBar = props => {
 						Via Uploads
 					</NavLink>
 				</li>
-				<li className="tab-group-imports tab-uploads ui-state-default ui-corner-top">
+				<li className="tab-group-imports">
 					<NavLink
 						exact
 						activeClassName="active"
@@ -86,13 +90,15 @@ const NavBar = props => {
 						Via FTP Uploads
 					</NavLink>
 				</li>
-				<NavLink exact className="children-tab-button" to="/dashboard/settings">Settings</NavLink>
-				<li className="tab-group-settings ui-state-default ui-corner-top">
+				<NavLink exact className="settings-tab-button" to="/dashboard/settings">
+					Settings
+				</NavLink>
+				<li className="tab-group-settings ">
 					<NavLink exact activeClassName="active" to="/dashboard/settings/past">
 						Authors
 					</NavLink>
 				</li>
-				<li className="tab-group-settings ui-state-default ui-corner-top">
+				<li className="tab-group-settings ">
 					<NavLink
 						exact
 						activeClassName="active"
