e6e3bfd91fdd5e1a4be4801503c31e946f6121cd
Carmezim <carmezim.filho@gmail.com>
2017-07-19 05:24:36 +0100

    updating headers

--- src/common/headers/ListsHeader.js
+++ src/common/headers/ListsHeader.js
@@ -8,13 +8,27 @@ const ListHeader = props => {
 			<div className="container-inner">
 				<div className="panel-body panel-titles">
 					<div className="panel-content">
-						<div className="pan-col col-same pan-author-title"><p>Author Name</p></div>
-						<div className="pan-col col-same pan-event-title"><p>Event Title</p></div>
-						<div className="pan-col pan-date"><p>Start Date</p></div>
-						<div className="pan-col col-same pan-price"><p>Price</p></div>
-						<div className="pan-col col-same pan-age"><p>Ages</p></div>
-						<div className="pan-col col-same pan-order"><p>Pre-order</p></div>
-						<div className="pan-col col-same"><p>Start Time</p></div>
+						<div className="pan-col col-same pan-author-title">
+							<p>Author Name</p>
+						</div>
+						<div className="pan-col col-same pan-event-title">
+							<p>Event Title</p>
+						</div>
+						<div className="pan-col pan-date">
+							<p>Start Date</p>
+						</div>
+						<div className="pan-col col-same pan-price">
+							<p>Price</p>
+						</div>
+						<div className="pan-col col-same pan-age">
+							<p>Ages</p>
+						</div>
+						<div className="pan-col col-same pan-order">
+							<p>Pre-order</p>
+						</div>
+						<div className="pan-col col-same">
+							<p>Start Time</p>
+						</div>
 					</div>
 				</div>
 			</div>
