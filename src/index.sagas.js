b68aadbbbb7068c418791bc4d718ece0b9263537
Carmezim <carmezim.filho@gmail.com>
2017-07-20 20:40:50 +0100

    adding dashboard watcher to index-sagas

--- src/index.sagas.js
+++ src/index.sagas.js
@@ -1,8 +1,9 @@
 import { all } from "redux-saga/effects";
 import SignupSaga from "./signup/sagas";
 import LoginSaga from "./login/sagas";
+import dashboardWatcher from "./dashboard/sagas";
 import dndWatcher from "./draggableList/sagas";
 
 export default function* IndexSaga() {
-	yield all([SignupSaga(), LoginSaga(), dndWatcher()]);
+	yield all([SignupSaga(), LoginSaga(), dashboardWatcher(), dndWatcher()]);
 }
