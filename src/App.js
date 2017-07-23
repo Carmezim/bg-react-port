305d41e44f278cc8abbd56e87f1cd5f9d4a30c73
Carmezim <carmezim.filho@gmail.com>
2017-07-23 03:57:50 +0100

    fixing rendering two components on same path with Switch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import PrivateRoute from "./common/authentication/AuthRoute";
 import { Route, withRouter } from "react-router-dom";
-import { reset} from "redux-form";
+import { reset } from "redux-form";
 
 // Import all components
 import Login from "./login";
@@ -20,7 +20,13 @@ import { eventCreate } from "./dashboard/actions";
 
 class App extends Component {
 	static propTypes = {
-		children: PropTypes.node
+		children: PropTypes.node,
+		eventCreate: PropTypes.func.isRequired,
+		reset: PropTypes.func.isRequired,
+		client: PropTypes.shape({
+			token: PropTypes.string
+		}).isRequired,
+		unsetClient: PropTypes.func.isRequired
 	};
 
 	// Populate initial state with
@@ -45,7 +51,6 @@ class App extends Component {
 
 	render() {
 		const { client: { token }, unsetClient } = this.props;
-
 		return (
 			<div className="App">
 				<div className="app-header">
@@ -62,25 +67,34 @@ class App extends Component {
 						<PrivateRoute
 							token={token}
 							isPrivate={true}
-							exact
+							exact={true}
 							path="/dashboard"
-							render={() => <Dashboard />}
+							component={Dashboard}
 						/>
 						<PrivateRoute
 							token={token}
 							isPrivate={true}
-							exact
-							path="/dashboard"
-							render={() => <NavBar />}
-						/>
-						<PrivateRoute
-							token={token}
-							isPrivate={true}
-							exact
+							exact={true}
 							path="/dashboard/create"
 							render={() => <EventTemplate onSubmit={this.handleSubmit} />}
 						/>
 						<Route exact path="/" component={Signup} />
+						<Route render={() => <div>Not Found :(</div>} />
+					</Switch>
+					{
+						// Switch matches the first occurrence of a path (url) so we need
+						// to add another 'Switch'  (or how many respectively to the number of components
+						// we need to render with the same url)
+						// Reference: https://github.com/ReactTraining/react-router/issues/3928#issuecomment-282495394
+					}
+					<Switch>
+						<PrivateRoute
+							token={token}
+							isPrivate={true}
+							exact={false}
+							path="/dashboard"
+							component={NavBar}
+						/>
 						<Route render={() => <div>Not Found :(</div>} />
 					</Switch>
 				</section>
@@ -91,11 +105,14 @@ class App extends Component {
 
 const mapStateToProps = state => ({
 	client: state.client,
-	draggable: state.draggable
+	draggable: state.draggable,
+	dashboard: state.dashboard
 });
 
 const AppWithRouter = withRouter(
-	connect(mapStateToProps, { fetchEvents, unsetClient, eventCreate, reset })(App)
+	connect(mapStateToProps, { fetchEvents, unsetClient, eventCreate, reset })(
+		App
+	)
 );
 
 export default AppWithRouter;
